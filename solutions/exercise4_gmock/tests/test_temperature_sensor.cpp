89f8b2a67ec1f5ae9395b89d67e689e22fac5f2c
Niall Cooling <niall.cooling@gmail.com>
2021-01-08 17:09:36 +0000

    added valgrind support

--- solutions/exercise4_gmock/tests/test_temperature_sensor.cpp
+++ solutions/exercise4_gmock/tests/test_temperature_sensor.cpp
@@ -1,12 +1,12 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
-using ::testing::Return;
 using ::testing::_;
-using ::testing::NiceMock;
-using ::testing::StrictMock;
-using ::testing::StartsWith;
 using ::testing::ContainsRegex;
+using ::testing::NiceMock;
+using ::testing::Return;
+using ::testing::StartsWith;
+using ::testing::StrictMock;
 
 #include "IDisplay.h"
 #include "ds1820.h"
@@ -14,94 +14,103 @@ using ::testing::ContainsRegex;
 
 class mock_display : public Display::IDisplay {
 public:
-  MOCK_METHOD1(display, int (const char* str));
+  MOCK_METHOD1(display, int(const char* str));
 };
 
-class Mock_tests : public ::testing::Test {
+class Mock_tests_initialise : public ::testing::Test {
 protected:
-  Mock_tests()          = default;
-  virtual ~Mock_tests() = default;
+  Mock_tests_initialise()          = default;
+  virtual ~Mock_tests_initialise() = default;
 
   // stub object
   Sensor::Ds1820 sensor{};
   // Create our mock object(s)
+  // NiceMock<mock_display> display{};
   StrictMock<mock_display> display{};
+
+  Temperature_sensor test_obj{ display, sensor };
 };
 
-TEST_F(Mock_tests, test_initialisation)
+TEST_F(Mock_tests_initialise, test_initialisation)
 {
   EXPECT_CALL(display, display(_)).Times(2);
 
-  Temperature_sensor test_obj{ display, sensor };
-
   auto status = test_obj.initialize();
   ASSERT_EQ(status, Temperature_sensor::status::ok);
 }
 
-TEST_F(Mock_tests, test_init_display_1st_failure)
+TEST_F(Mock_tests_initialise, test_init_display_1st_failure)
 {
   EXPECT_CALL(display, display(_)).WillOnce(Return(-1));
-
-  Temperature_sensor test_obj{ display, sensor };
 
   auto status = test_obj.initialize();
   ASSERT_EQ(status, Temperature_sensor::status::display_failure);
 }
 
-TEST_F(Mock_tests, test_init_display_2nd_failure)
+TEST_F(Mock_tests_initialise, test_init_display_2nd_failure)
 {
-  ::testing::InSequence strict_sequence; 
+  ::testing::InSequence strict_sequence;
 
   EXPECT_CALL(display, display(_)).WillOnce(Return(0));
   EXPECT_CALL(display, display(_)).WillOnce(Return(-1));
 
-  Temperature_sensor test_obj{ display, sensor };
-
   auto status = test_obj.initialize();
   ASSERT_EQ(status, Temperature_sensor::status::display_failure);
 }
 
-TEST_F(Mock_tests, test_run_display_no_failure)
-{
-  ::testing::InSequence strict_sequence; 
+class Mock_tests_run : public ::testing::Test {
+protected:
+  Mock_tests_run()
+  {
+    // EXPECT_CALL(display, display(_))
+    //   .Times(2); // only needed if StrictMock used
+    EXPECT_CALL(display, display(StartsWith("Family code: 0x")))
+      .WillOnce(Return(0));
+    EXPECT_CALL(display, display(StartsWith("Serial Number:")))
+      .WillOnce(Return(0));
 
-  EXPECT_CALL(display, display(_)).Times(3).WillRepeatedly(Return(0));
+    test_obj.initialize();
+  }
+
+  // stub object
+  Sensor::Ds1820 sensor{};
+  // Create our mock object(s)
+  // NiceMock<mock_display> display{};
+  StrictMock<mock_display> display{};
 
   Temperature_sensor test_obj{ display, sensor };
+};
 
-  test_obj.initialize();
+TEST_F(Mock_tests_run, test_run_display_no_failure)
+{
+  ::testing::InSequence strict_sequence;
+
+  EXPECT_CALL(display, display(_)).Times(1).WillRepeatedly(Return(0));
+
   auto status = test_obj.run();
   ASSERT_EQ(status, Temperature_sensor::status::ok);
 }
 
-TEST_F(Mock_tests, test_init_display_3nd_failure)
+TEST_F(Mock_tests_run, test_init_display_3nd_failure)
 {
-  ::testing::InSequence strict_sequence; 
+  ::testing::InSequence strict_sequence;
 
-  EXPECT_CALL(display, display(_)).WillOnce(Return(0));
-  EXPECT_CALL(display, display(_)).WillOnce(Return(0));
   EXPECT_CALL(display, display(_)).WillOnce(Return(-1));
 
-  Temperature_sensor test_obj{ display, sensor };
-
-  test_obj.initialize();
   auto status = test_obj.run();
   ASSERT_EQ(status, Temperature_sensor::status::display_failure);
 }
 
-
-
-TEST_F(Mock_tests, test_init_display_param_check)
+TEST_F(Mock_tests_run, test_init_display_param_check)
 {
-  ::testing::InSequence strict_sequence; 
+  ::testing::InSequence strict_sequence;
 
-  EXPECT_CALL(display, display(StartsWith("Family code: 0x"))).WillOnce(Return(0));
-  EXPECT_CALL(display, display(StartsWith("Serial Number:"))).WillOnce(Return(0));
+  // EXPECT_CALL(display, display(StartsWith("Family code: 0x")))
+  //   .WillOnce(Return(0));
+  // EXPECT_CALL(display, display(StartsWith("Serial Number:")))
+  //   .WillOnce(Return(0));
   EXPECT_CALL(display, display(ContainsRegex("..\\...C"))).WillOnce(Return(0));
 
-  Temperature_sensor test_obj{ display, sensor };
-
-  test_obj.initialize();
   auto status = test_obj.run();
   ASSERT_EQ(status, Temperature_sensor::status::ok);
 }
